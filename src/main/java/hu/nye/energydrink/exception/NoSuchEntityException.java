b24292b39a295c8be8c705b43a3ea24beff728af
DMilan05 <dmilan.felvi@gmail.com>
2025-04-30 09:49:08 +0200

    Fixed some errors and added Thymeleaf dependency.

---
+++ src/main/java/hu/nye/energydrink/exception/NoSuchEntityException.java
@@ -0,0 +1,12 @@
+package hu.nye.energydrink.exception;
+
+public class NoSuchEntityException extends RuntimeException {
+
+    public NoSuchEntityException(String message) {
+        super(message);
+    }
+
+    public NoSuchEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
